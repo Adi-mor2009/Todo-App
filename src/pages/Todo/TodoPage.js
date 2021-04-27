baa7b9d5a3b6db3d0ce66d5d272557acdafe6711
Adi-mor2009 <adijmy@gmail.com>
2021-04-27 23:41:21 +0300

    making it one todo list, added footer navbar, added icon on hover

--- src/pages/Todo/TodoPage.js
+++ src/pages/Todo/TodoPage.js
@@ -1,35 +1,92 @@
 import React from "react";
-import ActiveTasks from "../../components/ActiveTasks/ActiveTasks";
-import ComplitedTasks from "../../components/ComplitedTasks/ComplitedTasks";
-const { Container, Alert } = require("react-bootstrap");
+import Tasks from "../../components/Tasks/Tasks";
+import './Todo.css';
+const { Container, Modal, Button, Navbar, Row } = require("react-bootstrap");
 const { default: TaskBox } = require("../../components/TaskBox/TaskBox");
 
 function Todo() {
-    const [tasks, setTasks] = React.useState([]);
-    const [complitedTasks, SetComplitedTasks] = React.useState([]);
-    const [text, setText] = React.useState("");
+    const [tasks, setTasks] = React.useState([]); //[{ text: newTask, status: true }, { text: newTask, status: false }...]
+    const [text, setText] = React.useState("");//Of tasks Component
+    const [show, setShow] = React.useState("All");
     function handleEnterPressed(newTask) {
-        setTasks(tasks => tasks.concat(newTask))
+        setTasks(tasks => tasks.concat({ text: newTask, status: true }))
         setText("");
     }
     function handleTextChange(newText) {
         setText(newText);
     }
     function markTaskAsComplited(index) {
-        SetComplitedTasks(complitedTasks => [...complitedTasks, tasks[index]]);
-        setTasks(tasks => tasks.slice(0, index).concat(tasks.slice(index + 1, tasks.length)));
+        //setTasks(tasks => tasks => [...tasks, tasks[index].status]);
+        let newArr = tasks.map((task, i) => {
+            if (index == i) {
+                //return newArr[index].status = false;
+                let changedTask = { ...task };
+                changedTask.status = !changedTask.status;
+                return changedTask;
+            } else {
+                return task;
+            }
+        });
+        setTasks(newArr);
+        // SetComplitedTasks(complitedTasks => [...complitedTasks, tasks[index]]);
+        // setTasks(tasks => tasks.slice(0, index).concat(tasks.slice(index + 1, tasks.length)));
     }
-    function removeTaskFromActive(index) {
-        Alert();
+    function removeTask(index) {
+        if (tasks[index].status) {
+            return (
+                <Modal.Dialog>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Delete Action</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <p>Are you sure you want to delete your active task?</p>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="secondary">Close</Button>
+                        <Button variant="primary">Yes</Button>
+                    </Modal.Footer>
+                </Modal.Dialog>
+            );
+        }
+        else {
+            setTasks(tasks => tasks.slice(0, index).concat(tasks.slice(index + 1, tasks.length)));
+        }
     }
-    return(
+    function getNumOfActiveTasks() {
+        return tasks.filter(task => task.status).length;
+        //return activeTasksNum ? activeTasksNum + " items left" : "";
+    }
+    // function setShowStatus(status) {
+    //     setShow(status);
+    // }
+    function getTasks() {
+        switch(show) {
+            case "All":
+                return tasks;
+            case "Active":
+                return tasks.filter(task => task.status);
+            case "Complite":
+                return tasks.filter(task => !task.status); 
+        }
+    }
+    return (
         <div className="p-todo">
             <Container>
                 <h1 className="display-1">Todos</h1>
                 <TaskBox placeholder="Add task ... (to finish press enter)" onEnter={handleEnterPressed} taskText={text} onTextChange={handleTextChange}></TaskBox>
-                <ActiveTasks tasks={tasks} onTaskRemove={removeTaskFromActive} onTaskSelected={markTaskAsComplited}></ActiveTasks>
-                <ComplitedTasks tasks={complitedTasks} onTaskRemove={removeTaskFromActive} onTaskSelected={markTaskAsComplited}></ComplitedTasks>
+                <Tasks tasks={getTasks()} onTaskRemove={removeTask} onTaskSelected={markTaskAsComplited}></Tasks>
+                {/* <ComplitedTasks tasks={complitedTasks} onTaskRemove={removeTaskFromActive} onTaskSelected={markTaskAsComplited}></ComplitedTasks> */}
             </Container>
+            <Navbar className={tasks.length == 0 ? "hide" : ""}>
+                <Navbar.Brand>{getNumOfActiveTasks()} items left</Navbar.Brand>
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Brand className={show === "All" ? "nav-filter marked" : "nav-filter"} onClick={() => setShow("All")}>All</Navbar.Brand>
+                    <Navbar.Brand className={show === "Active" ? "nav-filter marked" : "nav-filter"} onClick={() => setShow("Active")}>Active</Navbar.Brand>
+                    <Navbar.Brand className={show === "Complite" ? "nav-filter marked" : "nav-filter"} onClick={() => setShow("Complite")}>Complite</Navbar.Brand>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     )
 }
